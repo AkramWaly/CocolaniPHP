58ebba72b1ecb142ac913ce9eaccd32a55693814
AkramWaly <57056713+AkramWaly@users.noreply.github.com>
2023-04-28 08:12:17 +0200

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_password = "qazqazqaz1";
+    $db_name = "cocolani"; 
+	
+	$connect = mysqli_connect($db_server,$db_user,$db_password,$db_name);
+
+
+?>
